8ecb8558ab4054a01bca261cefa2b360436b348c
Mr.Yes <yefajie@live.cn>
2018-09-14 14:39:13 +0800

    Update data.cpp

--- src/data.cpp
+++ src/data.cpp
@@ -11,13 +11,13 @@ Data::Data(const string& data_dir, int batch_size, const string& layer_name, con
     batch_size_ = batch_size;
     if (phase == "train") {
         cout << "Read train data..." << endl;
-        provider_ = make_shared<DataProvider>(data_dir+"/train-images.idx3-ubyte",
-                                              data_dir+"/train-labels.idx1-ubyte", shuffle);
+        provider_ = make_shared<DataProvider>(data_dir+"/train-images-idx3-ubyte",
+                                              data_dir+"/train-labels-idx1-ubyte", shuffle);
         cout << "Read train data done!!!" << endl << endl;
     } else {
         cout << "Read test data..." << endl;
-        provider_ = make_shared<DataProvider>(data_dir+"/t10k-images.idx3-ubyte",
-                                              data_dir+"/t10k-labels.idx1-ubyte", false);
+        provider_ = make_shared<DataProvider>(data_dir+"/t10k-images-idx3-ubyte",
+                                              data_dir+"/t10k-labels-idx1-ubyte", false);
         cout << "Read test data done!!!" << endl << endl;
     }
     cout << "Initialize data layer: " << layer_name_ << " done..." << endl;
