0b40285f6be02dc0675d9e47e57aadc9125358c5
Mr.Yes <yefajie@live.cn>
2019-05-13 21:56:33 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -9,190 +9,151 @@
 #include <memory>
 #include <iomanip>
 #include <fstream>
+#include <utility>
+#include <thread>
+#include <chrono>
+#include <string.h>
+#include <omp.h>
 
-#include "chunk.h"
-#include "layer.h"
-#include "convolution.h"
-#include "bias.h"
-#include "softmax.h"
-#include "softmaxloss.h"
-#include "dense.h"
-#include "relu.h"
-#include "argmax.h"
-#include "pooling.h"
-#include "accuracy.h"
-#include "data.h"
-#include "net.h"
-#include "util.h"
-#include "sgdoptimizer.h"
-#include "adagradoptimizer.h"
-#include "rmsproboptimizer.h"
-#include "adamoptimizer.h"
+#include "micronet.h"
 
 using namespace std;
+using namespace micronet;
+using json = nlohmann::json;
 
 
-void print(const Chunk& chunk, const string& name) {
-    const float* data = chunk.const_data();
-    const float* diff = chunk.const_diff();
-    cout << name << " shape" << endl;
-    cout << chunk.str_shape() << endl;
-    if (name == "loss" || name == "accuracy") {
-        cout << "data" << endl;
-        for (int i = 0; i < chunk.count(); ++i) {
-            cout << data[i] << " ";
-        }
-        cout << endl << "diff" << endl;
-        for (int i = 0; i < chunk.count(); ++i) {
-            cout << diff[i] << " ";
+pair<vector<data_t>, vector<data_t>> read_mnist_data() {
+    pair<vector<data_t>, vector<data_t>> mnist_data;
+    data_t train_images, train_labels;
+    read_mnist_images("data/mnist/train-images.idx3-ubyte", train_images);
+    read_mnist_lables("data/mnist/train-labels.idx1-ubyte", train_labels);
+    for (auto& image: train_images) {
+        for (float& pix: image) {
+            pix /= 255.0;
         }
     }
-    cout << endl << endl;
+    mnist_data.first.push_back(std::move(train_images));
+    mnist_data.first.push_back(std::move(train_labels));
+
+    data_t valid_images, valid_labels;
+    read_mnist_images("data/mnist/t10k-images.idx3-ubyte", valid_images);
+    read_mnist_lables("data/mnist/t10k-labels.idx1-ubyte", valid_labels);
+    for (auto& image: valid_images) {
+        for (float& pix: image) {
+            pix /= 255.0;
+        }
+    }
+    mnist_data.second.push_back(std::move(valid_images));
+    mnist_data.second.push_back(std::move(valid_labels));
+
+    return mnist_data;
 }
 
-void print_loss_acc(int iter, const Chunk& loss, const Chunk& acc, double time_per_iter) {
-    const float* loss_data = loss.const_data();
-    const float* acc_data = acc.const_data();
-    cout << "iter: " << setw(5) << iter << "\tloss: " << fixed <<setprecision(5) <<
-        loss_data[0] << "\tacc: " << fixed << setprecision(4) << acc_data[0]  <<
-        "\ttime_per_iter: " << fixed << setprecision(4) << (time_per_iter * 1000) << " ms" << endl;
+data_t get_noise_data(int noise_dim, int noise_num) {
+    data_t noise_data;
+    for (int i = 0; i < noise_num; ++i) {
+        vector<float> noise(noise_dim);
+        normal_random_init(noise_dim, noise.data(), 0.0f, 0.1f);
+        noise_data.push_back(noise);
+    }
+    return noise_data;
 }
 
-void write_to_file(map<string, vector<float>>& acc_map, map<string, vector<float>>& loss_map,
-                   string optimizer) {
-    ofstream acc_file(optimizer + "_acc_file.txt", std::ios::out);
-    ofstream loss_file(optimizer + "_loss_file.txt", std::ios::out);
-    acc_file << "train";
-    for (float acc: acc_map["train"]) {
-        acc_file << " " << acc;
+void save_mnist_imgs(map<string, data_t>& train_data, map<string, data_t>& valid_data) {
+    auto& train_imgs = train_data["real"];
+    auto& valid_imgs = valid_data["real"];
+    json j_imgs;
+    j_imgs["train"] = {};
+    for (int i = 0; i < train_imgs.size(); ++i) {
+        j_imgs["train"].push_back(train_imgs[i]);
     }
-    acc_file << endl;
-    acc_file << "test";
-    for (float acc: acc_map["test"]) {
-        acc_file << " " << acc;
+    j_imgs["valid"] = {};
+    for (int i = 0; i < valid_imgs.size(); ++i) {
+        j_imgs["valid"].push_back(valid_imgs[i]);
     }
-
-    loss_file << "train";
-    for (float loss: loss_map["train"]) {
-        loss_file << " " << loss;
-    }
-    loss_file << endl;
-    loss_file << "test";
-    for (float loss: loss_map["test"]) {
-        loss_file << " " << loss;
-    }
-    acc_file.close();
-    loss_file.close();
+    ofstream ofs("mnist_imgs.json");
+    ofs << j_imgs << endl;
+    cout << "save mnist imgs done...";
+    exit(0);
 }
 
-int main()
-{
-    Net net("LeNet5");
+chunk_ptr graph_constructor(const chunk_ptr& img) {
+    auto conv1 = Convolution(5, 5, 1, 1, 6, "valid")(img);
+    //auto batch1 = BatchNormalization()(conv1);
+    auto relu1 = Activation("relu")(conv1);
+    auto pool1 = Pooling(2, 2, 2, 2, "valid", "max")(relu1);
 
-    shared_ptr<Layer> data_train(new Data("data", 32, "data_train_layer", "train"));
-    data_train->set_chunks(vector<string>{"null"}, vector<string>{"img", "label"});
-    net.add_layer(data_train);
+    auto conv2 = Convolution(5, 5, 1, 1, 16, "valid")(pool1);
+    //auto batch2 = BatchNormalization()(conv2);
+    auto relu2 = Activation("relu")(conv2);
+    auto pool2 = Pooling(2, 2, 2, 2, "valid", "max")(relu2);
 
-    shared_ptr<Layer> data_test(new Data("data", 100, "data_test_layer", "test"));
-    data_test->set_chunks(vector<string>{"null"}, vector<string>{"img", "label"});
-    net.add_layer(data_test);
+    auto dense1 = Dense(120)(pool2);
+    //auto batch3 = BatchNormalization()(dense1);
+    auto relu3 = Activation("relu")(dense1);
+    auto dense2 = Dense(84)(relu3);
+    //auto batch4 = BatchNormalization()(dense2);
+    auto relu4 = Activation("relu")(dense2);
+    auto output = Dense(10)(relu4);
 
-    shared_ptr<Layer> conv1(new Convolution(5, 5, 2, 2, 1, 1, 1, 6, "conv1_layer"));
-    conv1->set_chunks(vector<string>{"img"}, vector<string>{"conv1"});
-    net.add_layer(conv1);
+    return output;
+}
 
-    shared_ptr<Layer> relu1(new ReLU("relu1_layer"));
-    relu1->set_chunks(vector<string>{"conv1"}, vector<string>{"relu1"});
-    net.add_layer(relu1);
+chunk_ptr generator_constructor(const chunk_ptr& noise) {
+    auto dense1 = Dense(256)(noise);
+    auto batch1 = BatchNormalization()(dense1);
+    auto relu1 = Activation("leaky_relu")(batch1);
+    auto dense2 = Dense(256)(relu1);
+    auto batch2 = BatchNormalization()(dense2);
+    auto relu2 = Activation("leaky_relu")(batch2);
+    auto dense3 = Dense(28*28)(relu2);
+    auto output = Activation("sigmoid")(dense3);
 
-    shared_ptr<Layer> pool1(new Pooling(2, 2, 0, 0, 2, 2, "pool1_layer", "avg"));
-    pool1->set_chunks(vector<string>{"relu1"}, vector<string>{"pool1"});
-    net.add_layer(pool1);
+    return output;
+}
 
-    shared_ptr<Layer> conv2(new Convolution(5, 5, 0, 0, 1, 1, 6, 16, "conv2_layer"));
-    conv2->set_chunks(vector<string>{"pool1"}, vector<string>{"conv2"});
-    net.add_layer(conv2);
+chunk_ptr discriminator_constructor(const chunk_ptr& real) {
+    auto dense1 = Dense(256)(real);
+    auto relu1 = Activation("leaky_relu")(dense1);
+    auto dense2 = Dense(256)(relu1);
+    auto relu2 = Activation("leaky_relu")(dense2);
+    auto output = Dense(1)(relu2);
 
-    shared_ptr<Layer> relu2(new ReLU("relu2_layer"));
-    relu2->set_chunks(vector<string>{"conv2"}, vector<string>{"relu2"});
-    net.add_layer(relu2);
+    return output;
+}
 
-    shared_ptr<Layer> pool2(new Pooling(2, 2, 0, 0, 2, 2, "pool2_layer", "avg"));
-    pool2->set_chunks(vector<string>{"relu2"}, vector<string>{"pool2"});
-    net.add_layer(pool2);
+int main() {
+    // Train Gans
+    /*pair<vector<data_t>, vector<data_t>> mnist_data = read_mnist_data();
+    map<string, data_t> train_data = {{"real", std::move(mnist_data.first[0])}};
+    map<string, data_t> valid_data = {{"real", std::move(mnist_data.second[0])}};
 
-    shared_ptr<Layer> dense1(new Dense(16*5*5, 120, "dense1_layer"));
-    dense1->set_chunks(vector<string>{"pool2"}, vector<string>{"dense1"});
-    net.add_layer(dense1);
+    GanNet2 net(generator_constructor, 50, discriminator_constructor, {28*28});
+    net.print_net();
+    net.set_optimizer(make_shared<AdamOptimizer>(0.001, vector<float>{0.5, 0.75}));
+    net.fit(train_data, valid_data, 64, 48, 100, true);
+    net.save_model("gannet.json");
 
-    shared_ptr<Layer> relu3(new ReLU("relu3_layer"));
-    relu3->set_chunks(vector<string>{"dense1"}, vector<string>{"relu3"});
-    net.add_layer(relu3);
+    /*GanNet2 net;
+    net.load_model("gannet.json");
+    map<string, data_t> noise_data = {{"noise", std::move(get_noise_data(50, 100))}};
+    data_t infer_data = net.inference(noise_data, 100);
+    json j_infer_data = infer_data;
+    ofstream ofs("gan_imgs/infer_imgs.json");
+    ofs << j_infer_data << endl;
+    //net.fit(train_data, valid_data, 64, 48, 100, true);*/
 
-    shared_ptr<Layer> dense2(new Dense(120, 84, "dense2_layer"));
-    dense2->set_chunks(vector<string>{"relu3"}, vector<string>{"dense2"});
-    net.add_layer(dense2);
 
-    shared_ptr<Layer> relu4(new ReLU("relu4_layer"));
-    relu4->set_chunks(vector<string>{"dense2"}, vector<string>{"relu4"});
-    net.add_layer(relu4);
+    // Train class
+    pair<vector<data_t>, vector<data_t>> mnist_data = read_mnist_data();
+    map<string, data_t> train_data = {{"img", std::move(mnist_data.first[0])}, {"label", std::move(mnist_data.first[1])}};
+    map<string, data_t> valid_data = {{"img", std::move(mnist_data.second[0])}, {"label", std::move(mnist_data.second[1])}};
 
-    shared_ptr<Layer> output(new Dense(84, 10, "output_layer"));
-    output->set_chunks(vector<string>{"relu4"}, vector<string>{"output"});
-    net.add_layer(output);
+    ClassifyNet net(graph_constructor, {28, 28, 1});
+    net.set_optimizer(make_shared<AdamOptimizer>(0.001, vector<float>{0.5, 0.75}));
+    net.fit(train_data, valid_data, 32, 12, 100, true);
+    net.save_model("net_mnist.json");
 
-    shared_ptr<Layer> softmaxloss(new SoftmaxLoss("softmaxloss_layer"));
-    softmaxloss->set_chunks(vector<string>{"output", "label"}, vector<string>{"loss", "prob"});
-    net.add_layer(softmaxloss);
-
-    shared_ptr<Layer> accuracy(new Accuracy("accuracy_layer"));
-    accuracy->set_chunks(vector<string>{"prob", "label"}, vector<string>{"accuracy"});
-    net.add_layer(accuracy);
-
-    net.set_data_layers(vector<string>{"data_train_layer", "data_test_layer"});
-    net.initialize();
-    net.set_default_data_layer("data_train_layer");
-
-    shared_ptr<Optimizer> optimizer(new AdamOptimizer(0.001, vector<int>{6000, 8000}));
-    net.set_optimizer(optimizer);
-
-    map<string, vector<float>> acc_map;
-    map<string, vector<float>> loss_map;
-
-    int test_interval = 100;
-    for (int i = 0; i < 10000; ++i) {
-        auto time_point1 = std::chrono::high_resolution_clock::now();
-
-        net.forward();
-        net.backward();
-        net.update();
-
-        auto time_point2 = std::chrono::high_resolution_clock::now();
-        auto time_span = std::chrono::duration_cast<std::chrono::duration<double>>(time_point2 - time_point1);
-
-        if (i % 10 == 0) {
-            print_loss_acc(i, net.all_chunks_["loss"], net.all_chunks_["accuracy"], time_span.count());
-            acc_map["train"].push_back(net.all_chunks_["accuracy"].const_data()[0]);
-            loss_map["train"].push_back(net.all_chunks_["loss"].const_data()[0]);
-        }
-        if (i % test_interval == 0) {
-            net.set_default_data_layer("data_test_layer");
-            float test_acc = 0;
-            float test_loss = 0;
-            for (int j = 0; j < 100; ++j) {
-                net.forward();
-                test_acc += net.all_chunks_["accuracy"].const_data()[0];
-                test_loss += net.all_chunks_["loss"].const_data()[0];
-            }
-            test_acc /= 100;
-            test_loss /= 100;
-            acc_map["test"].push_back(test_acc);
-            loss_map["test"].push_back(test_loss);
-            cout << "-------------------------- test acc: " << test_acc << "---------------------------" << endl;
-            net.set_default_data_layer("data_train_layer");
-        }
-    }
-    write_to_file(acc_map, loss_map, "adam");
 
     return 0;
 }
