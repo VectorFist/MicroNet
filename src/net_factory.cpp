02ef6611d689ecd0f9711a6e60f7292a88ae2023
Mr.Yes <yefajie@live.cn>
2019-05-13 21:59:04 +0800

    Delete net_factory.cpp

--- src/net_factory.cpp
+++
@@ -1,9 +0,0 @@
-#include "net_factory.h"
-
-namespace micronet {
-
-Net build_resnet18(const chunk_ptr& img) {
-    auto conv1 = Convolution(3, 3, 1, 1, 1, 1, 64)(img);
-}
-
-} // namespace micronet
